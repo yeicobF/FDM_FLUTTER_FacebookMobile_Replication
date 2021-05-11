137975294e95a067665e2b2f4946e5e1ae7419fd
yeicobF <60598673+yeicobF@users.noreply.github.com>
2021-05-11 14:04:11 -0500

    LOGIN 1 - BOTONES CON ÍCONO FUNCIONALES
    - Ya funcionan los botones que tienen íconos, pero aún no tienen
      establecida la función para cambiar de pantalla. Eso lo hago después.

--- lib/screens/main_feed_screen.dart
+++ lib/screens/main_feed_screen.dart
@@ -1,2 +1,16 @@
 // En este archivo se encontrará la Feed principal de Facebook después de haber
 // pasado por las dos pantallas de inicio de sesión.
+
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class MainFeed extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 50,
+      height: 50,
+      color: Colors.amber[200],
+    );
+  }
+}
