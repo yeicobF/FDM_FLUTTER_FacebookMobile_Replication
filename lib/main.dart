160b4d073dcfb317dd48c963f4b70299c710e81c
yeicobF <60598673+yeicobF@users.noreply.github.com>
2021-05-21 19:34:23 -0500

    CLASE PROVISIONAL PARA PANTALLAS
    Creé una clase provisional para manejar las pantallas.
    
    - POR HACER:
    
            - Habilitar el null safety cambiando la versión del SDK de
              Flutter.

--- lib/main.dart
+++ lib/main.dart
@@ -23,4 +23,39 @@ import 'screens/main_feed_screen.dart' as main_feed_screen;
 
 // void main() => runApp(MaterialApp(home: first_login_screen.FirstLoginScreen()));
 // void main() => runApp(MaterialApp(home: login_screen_after_click.LoginScreenAfterClick()));
-void main() => runApp(MaterialApp(home: main_feed_screen.MainFeedScreen()));
+void main() => runApp(FacebookScreens(0));
+
+// CLASE PROVISIONAL PARA MOSTRAR LAS PANTALLAS QUE HEMOS HECHO DEPENDIENDO DEL
+// NÚMERO QUE RECIBA COMO PARÁMETRO.
+class FacebookScreens extends StatelessWidget {
+  // El número de la pantalla.
+  final int screenNumber;
+  // Las pantallas.
+  final List<Widget> screens = [
+    first_login_screen.FirstLoginScreen(),
+    login_screen_after_click.LoginScreenAfterClick(),
+    main_feed_screen.MainFeedScreen(),
+  ];
+  
+  FacebookScreens(this.screenNumber);
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    // Si el número de pantalla es null, < 0, o mayor al número de pantallas,
+    // hacerlo 0.
+    final int screenToShow = (screenNumber < 0
+                              || screenNumber > (screens.length - 1)
+                              || screenNumber == null)
+                              ? 0
+                              : screenNumber;
+
+    return MaterialApp(
+      // PARA ELIMINAR LA ETIQUETA DE LA PARTE SUPERIOR DERECHA QUE INDICA 
+      // "Debug".
+      home: screens[screenToShow],
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
